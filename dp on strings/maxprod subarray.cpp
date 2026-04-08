b78c6295a0373346d0426755d1fa676410713b82
Nitya Choudhary <104771859+Nitya-Choudhary@users.noreply.github.com>
2026-04-08 17:32:55 +0530

    Create maxprod subarray.cpp

---
+++ dp on strings/maxprod subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n= nums.size();
+        int maxprod= nums[0];
+        for(int i=0;i<n;i++){
+            int prod=1;
+            for(int j=i;j<n;j++){
+                prod= prod*nums[j];
+                maxprod= max(maxprod, prod);
+            }
+        }
+        return maxprod;
+    }
+};
