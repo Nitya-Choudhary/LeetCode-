54076aa73950f3077b2abb385c73a4f28eeae3b2
Nitya Choudhary <104771859+Nitya-Choudhary@users.noreply.github.com>
2026-04-12 22:34:23 +0530

    Create LCA.cpp

---
+++ BST/LCA.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+     Node* LCA(Node* root, Node* p, Node* q){
+       if(root->nullptr ||root==p|| root==q) return root;
+        }
+        Node* left = lowestCommonAncestor(root->left, p, q);
+        Node* right = lowestCommonAncestor(root->right, p, q);
+        if (left != nullptr && right != nullptr) {
+            return root;
+        }
+        return (left != nullptr ? left : right);
+     }
+}
