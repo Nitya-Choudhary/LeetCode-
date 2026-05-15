76bcef52410cf435fe225062f580c7fba4fc6e71
Nitya Choudhary <104771859+Nitya-Choudhary@users.noreply.github.com>
2026-05-15 23:09:33 +0530

    Create median_two_arrays.cpp

---
+++ median_two_arrays.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm> // For std::sort
+#include <iostream>   // For std::cout (if testing)
+
+using namespace std;
+
+class Solution{
+public:
+      double median(vector<int>nums1, vector<int>nums2){
+        vector<int>merged;
+        for(int x:nums1){
+          merged.push_back(x);
+        }
+        for(int x:nums2){
+          merged.push_back(x);
+        }
+        sort(merged.begin(), merged.end());
+        int n= merged.size();
+        if(n%2==1){
+          return merged[n/2];
+          
+        }
+        else{
+          int mid1=merged[n/2-1];
+          int mid2= merged[n/2];
+          return (double)(mid1+mid2)/2;
+        }
+      }
+};
