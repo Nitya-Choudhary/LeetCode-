5eb80b5ba33ed51d3696ddc3d1f9f5d1d9578378
Nitya Choudhary <104771859+Nitya-Choudhary@users.noreply.github.com>
2026-06-04 14:10:42 +0530

    Create Number of Islaands.cpp

---
+++ Graph/Number of Islaands.cpp
@@ -0,0 +1,25 @@
+int numislands(vector<vector<int>>& grid){
+  int n= grid.size();
+  int m= grid[0].size();
+  int islands=0;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(grid[i][j]=='1'){
+        islands++;
+        dfs(grid,i,j,m,n);
+      }
+    }
+  }
+  return islands;
+}
+
+void dfs(vector<vector<int>>&grid, int i, int j, int m, int n){
+          if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') return ;
+  grid[i][j]='0';
+  dfs(grid,i+1,j,m,n);
+  dfs(grid,i,j+1,mn,n);
+  dfs(grid,i-1,j,m,n);
+  dfs(grid,i,j-1,m,n);
+  
+
+}
