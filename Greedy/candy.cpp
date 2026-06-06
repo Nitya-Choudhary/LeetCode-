1cecb5dbbfefe667aece1153881bb3a35c569a3d
Nitya Choudhary <104771859+Nitya-Choudhary@users.noreply.github.com>
2026-06-06 15:47:35 +0530

    Create candy.cpp

---
+++ Greedy/candy.cpp
@@ -0,0 +1,21 @@
+int candy(vector<int>& ratings){
+  int n= ratings.size();
+  if(n==0) return 0;
+  vector<int>left(n,1);
+  vector<int>right(n,1);
+  for(int i=1;i<n;++i){
+    if(ratings[i]>ratings[i-1]){
+      left[i]= left[i-1]+1;
+    }
+  }
+  for(int i=n-2;i>=0;--i){
+    if(ratings[i]>ratings[i+1]){
+      right[i]= right[i+1]+1;
+    }
+  }
+  int total=0;
+  for(int i=0;i<n;i++){
+    total= total+ max(right[i],left[i]);
+  }
+  return total;
+}
