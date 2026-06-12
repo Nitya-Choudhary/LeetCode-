426b23b4340e28c1827dcff93d72b7a52730daf8
Nitya Choudhary <104771859+Nitya-Choudhary@users.noreply.github.com>
2026-06-12 14:23:46 +0530

    Create 1 transaction.cpp

---
+++ Stocks/1 transaction.cpp
@@ -0,0 +1,9 @@
+int profit(vector<int>&prices){
+  int minprice= INT_MAX;
+  int profit=0;
+  for(int price:prices){
+    minprice= min(minprice, price);
+    profit= max(profit, price-minprice);
+  }
+  return profit;
+}
